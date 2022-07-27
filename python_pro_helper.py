d8c0c8d7bab668837276452af15667f0660114fe
Bernardo Lanza <87471233+bernardolanza93@users.noreply.github.com>
2022-07-27 12:24:44 +0200

    Update python_pro_helper.py

--- python_pro_helper.py
+++ python_pro_helper.py
@@ -4,3 +4,6 @@ import time
 start_time = time.time()
 main()
 print("--- %s seconds ---" % (time.time() - start_time))
+
+image = cv2.resize(image,(int(image.shape[1] / 4), int(image.shape[0] / 4)))
+
